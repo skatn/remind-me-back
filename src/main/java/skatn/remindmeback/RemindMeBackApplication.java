782aed2c2d501144778da02bc96b64b4247d2081
skatn <87296704+skatn@users.noreply.github.com>
2024-06-05 11:58:01 +0900

    Project init

---
+++ src/main/java/skatn/remindmeback/RemindMeBackApplication.java
@@ -0,0 +1,13 @@
+package skatn.remindmeback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindMeBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemindMeBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skatn/remindmeback/RemindMeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package skatn.remindmeback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindMeBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
