a5de87b104a3529814846c37758f1793120ecf23
skatn <87296704+skatn@users.noreply.github.com>
2024-08-06 16:06:31 +0900

    feat: front 배포 url cors 허용 (#32)

--- src/main/java/skatn/remindmeback/common/config/WebConfig.java
+++ src/main/java/skatn/remindmeback/common/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000")
+                .allowedOriginPatterns("http://localhost:3000", "https://remind-me-front.vercel.app")
                 .allowedMethods("*");
     }
 }
